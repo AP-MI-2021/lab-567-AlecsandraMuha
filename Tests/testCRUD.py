6fea9937be8062e8d0b1f0ab31e01d31cd72cde2
AlecsandraMuha <alecsandramuha@yahoo.com>
2021-10-26 19:59:46 +0300

    4.3

--- Tests/testCRUD.py
+++ Tests/testCRUD.py
@@ -1,5 +1,5 @@
 from Domain.librarie import getId, getTitlucarte, getGencarte, getPret, getTipReducere
-from Logic.CRUD import adaugaVanzare, getById
+from Logic.CRUD import adaugaVanzare, getById, stergeVanzare, modificaVanzare
 
 
 def testAdaugaVanzare():
@@ -22,3 +22,14 @@ def testStergeVanzare():
     lista = stergeVanzare("3",lista)
     assert len(lista) == 1
     assert getById("2", lista) is not None
+def testModificaVanzare():
+    lista = []
+    lista = adaugaVanzare("1", "Harap-Alb", "Basm", 15, "None",lista)
+    lista = adaugaVanzare("2", "Ion", "Roman", 10, "None", lista)
+    lista = modificaVanzare("2", "Ion", "Roman realist", 10, "Gold", lista)
+    assert len(lista) == 2
+    assert getId(getById("2", lista)) == "2"
+    assert getTitlucarte(getById("2", lista)) == "Ion"
+    assert getGencarte(getById("1", lista)) == "Roman realist"
+    assert getPret(getById("1", lista)) == 10
+    assert getTipReducere(getById("1", lista)) == "Gold"
