f028ec7133f9d4a9504bd3c703d17484a22d82c5
AlecsandraMuha <alecsandramuha@yahoo.com>
2021-11-03 19:44:24 +0200

    lab 6

--- UserInterface/fisiernou.py
+++ UserInterface/fisiernou.py
@@ -23,20 +23,20 @@ def comenzi(lista):
                     if comanda[0] == "add":
                         if len(comanda) != 6:
                             raise ValueError("Datele nu sunt introduse corect! ")
-                        id = comanda[1]
+                        id = int(comanda[1])
                         titlucarte = comanda[2]
                         gencarte = comanda[3]
                         pret = float(comanda[4])
                         tipreducere= comanda[5]
                         lista = creeazaVanzare(id, titlucarte, gencarte, pret, tipreducere)
                     elif comanda[0] == "delete":
-                        id = comanda[1]
+                        id = int(comanda[1])
                         lista = stergeVanzare(id, lista)
                         print("Obiectul a fost sters")
                     elif comanda[0] == "update":
                         if len(comanda) != 6:
                             raise ValueError("Datele nu sunt introduse corect! ")
-                        id = comanda[1]
+                        id = int(comanda[1])
                         titlucarte = comanda[2]
                         gencarte = comanda[3]
                         pret = float(comanda[4])
